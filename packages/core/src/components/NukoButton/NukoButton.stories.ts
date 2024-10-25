93d4126a2ec572c64d9dc11664cd6946ed811e26
takuma-ru <kanekotakuma@team-lab.com>
2024-10-25 16:03:24 +0900

    ♻️ Storybook file format

--- packages/core/src/components/NukoButton/NukoButton.stories.ts
+++ packages/core/src/components/NukoButton/NukoButton.stories.ts
@@ -1,29 +1,29 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
+import type NukoButtonCe from "./NukoButton.ce.vue";
 import { resisterElement } from "../../main";
 
 // This default export determines where your story goes in the story list
 const meta: Meta = {
   component: "nuko-button",
-};
-
-export default meta;
-type Story = StoryObj;
-
-export const FirstStory: Story = {
-  args: {
-    slot: "Default Slot Content",
-    attr: { type: "button" },
-  },
-  render: ({ slot, attr }) => {
+  render: (attr) => {
     resisterElement("nuko-button");
 
     const element = document.createElement("nuko-button");
-    element.innerHTML = slot;
+    element.innerHTML = "slot content";
 
-    Object.keys(attr).forEach((key) => {
-      element.setAttribute(key, attr[key]);
-    });
+    for (const key in attr) {
+      element.setAttribute(key, (attr as Record<string, any>)[key]);
+    }
 
     return element;
+  },
+};
+
+export default meta;
+type Story = StoryObj<InstanceType<typeof NukoButtonCe>["$props"]>;
+
+export const FirstStory: Story = {
+  args: {
+    type: "button",
   },
 };
