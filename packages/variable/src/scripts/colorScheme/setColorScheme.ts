5212e7730f9243eec1a99c0b61b5073f7371192d
takuma-ru <kanekotakuma@team-lab.com>
2025-02-04 16:43:27 +0900

    ✨ Update set color scheme

--- packages/variable/src/scripts/colorScheme/setColorScheme.ts
+++ packages/variable/src/scripts/colorScheme/setColorScheme.ts
@@ -7,5 +7,9 @@
 export const setColorScheme = (scheme: "dark" | "light" | "system") => {
   const htmlElement = document.documentElement;
 
+  if (scheme === "system") {
+    htmlElement.removeAttribute("data-color-scheme");
+  }
+
   htmlElement.setAttribute("data-color-scheme", scheme);
 };
