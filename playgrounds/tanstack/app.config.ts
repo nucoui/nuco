a3550f6925852a5d4bf2f5d596a4edaa2df945df
takuma-ru <kanekotakuma@team-lab.com>
2025-02-18 18:20:19 +0900

    🐛 Add config in pg:tanstack

--- playgrounds/tanstack/app.config.ts
+++ playgrounds/tanstack/app.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from '@tanstack/start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { cloudflare } from 'unenv'
 
 export default defineConfig({
+  server: {
+    preset: 'cloudflare-pages',
+    unenv: cloudflare,
+  },
   vite: {
     plugins: [
       tsConfigPaths({
