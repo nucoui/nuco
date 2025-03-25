8410756a499c73f8ece55e5badfc161a5d555e9b
takuma-ru <kanekotakuma@team-lab.com>
2025-03-25 11:19:11 +0900

    ✨ new color prefers

--- packages/variable/src/css/generators/generatePrefersColorSchemeCssVariables.ts
+++ packages/variable/src/css/generators/generatePrefersColorSchemeCssVariables.ts
@@ -67,7 +67,55 @@ export const generatePrefersColorSchemeCssVariables = (cssVariables: CssVariable
     },
   ];
 
-  const values = [...mappedPrefersColorSchemeCssVariables, ...mappedNeutralCssVariables];
+  const mappedAppCssVariables: prefersColorSchemeCssVariablesValue = [
+    {
+      key: "--cs-app-100",
+      dark: "var(--p-app-900)",
+      light: "var(--p-app-100)",
+    },
+    {
+      key: "--cs-app-200",
+      dark: "var(--p-app-800)",
+      light: "var(--p-app-200)",
+    },
+    {
+      key: "--cs-app-300",
+      dark: "var(--p-app-700)",
+      light: "var(--p-app-300)",
+    },
+    {
+      key: "--cs-app-400",
+      dark: "var(--p-app-600)",
+      light: "var(--p-app-400)",
+    },
+    {
+      key: "--cs-app-500",
+      dark: "var(--p-app-500)",
+      light: "var(--p-app-500)",
+    },
+    {
+      key: "--cs-app-600",
+      dark: "var(--p-app-400)",
+      light: "var(--p-app-600)",
+    },
+    {
+      key: "--cs-app-700",
+      dark: "var(--p-app-300)",
+      light: "var(--p-app-700)",
+    },
+    {
+      key: "--cs-app-800",
+      dark: "var(--p-app-200)",
+      light: "var(--p-app-800)",
+    },
+    {
+      key: "--cs-app-900",
+      dark: "var(--p-app-100)",
+      light: "var(--p-app-900)",
+    },
+  ];
+
+  const values = [...mappedPrefersColorSchemeCssVariables, ...mappedNeutralCssVariables, ...mappedAppCssVariables];
 
   const outputDir = path.resolve(__dirname, "../../../dist/css");
   if (!fs.existsSync(outputDir)) {
