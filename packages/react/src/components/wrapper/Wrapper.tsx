261d6ba197615ae0e01089b16c27ceb0ad227bd5
takuma-ru <kanekotakuma@team-lab.com>
2025-07-10 11:10:09 +0900

    🔥 Remove outdated `hastToJsx` utility and update package homepage
    ⚡️ Upgrade `@chatora/react` dependency to version 1.0.9

--- packages/react/src/components/wrapper/Wrapper.tsx
+++ packages/react/src/components/wrapper/Wrapper.tsx
@@ -4,12 +4,13 @@ import type { toReactEmits } from "@chatora/react";
 import type Elements from "@nuco/core/elements/elements";
 import type { ComponentEmits, ComponentProps } from "chatora";
 import type { HTMLAttributes, PropsWithChildren, ReactNode } from "react";
-import { hastToJsx } from "@/utils/hastToJsx";
+import { disableError, hastToJsx, splitProps } from "@chatora/react";
 import CustomElements from "@nuco/core/elements/customElements";
 import DeclarativeCustomElements from "@nuco/core/elements/declarativeCustomElements";
 import { useEffect, useId, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { jsx } from "react/jsx-runtime";
-import { renderToString } from "react-dom/server";
+
+disableError();
 
 export type Props<T extends keyof typeof CustomElements = keyof typeof CustomElements> = PropsWithChildren<{
   props: ComponentProps<typeof Elements[T]> & toReactEmits<ComponentEmits<typeof Elements[T]>> & {
@@ -19,33 +20,13 @@ export type Props<T extends keyof typeof CustomElements = keyof typeof CustomEle
   tag: T;
   formAssociated?: boolean;
 }>;
-
-const splitProps = (props: Record<string, unknown>) => {
-  const emits: Record<`on${string}`, (event: Event) => void> = {};
-  const filteredProps: Record<string, unknown> = {};
-  for (const [key, value] of Object.entries(props)) {
-    if (key.startsWith("on") && key.length > 2 && key[2] === key[2].toUpperCase() && typeof value === "function") {
-      const sliced = key.slice(2);
-      const normalized = sliced.charAt(0).toLowerCase() + sliced.slice(1);
-      const kebabKey = `onon-${normalized}`
-        .replace(/([A-Z])/g, "-$1")
-        .toLowerCase();
-      emits[kebabKey as `on${string}`] = value as (event: Event) => void;
-    }
-    else {
-      filteredProps[key] = value;
-    }
-  }
-  return { props: filteredProps, emits };
-};
-
 export const ChatoraWrapper = <T extends keyof typeof CustomElements>({ tag, children, props, formAssociated = false }: Props<T>) => {
   const [isDefined, setIsDefined] = useState(false);
 
   const id = useId();
   const { props: filteredProps, emits } = useMemo(() => splitProps(props || {}), [props]);
   const hast = DeclarativeCustomElements[tag](filteredProps as any);
-  const [content, setContent] = useState<ReactNode>(hastToJsx(tag, id, hast, [children]));
+  const [content, setContent] = useState<ReactNode>([hastToJsx(hast, `${id}-content`), children]);
   const domRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
@@ -72,10 +53,8 @@ export const ChatoraWrapper = <T extends keyof typeof CustomElements>({ tag, chi
 
   if (!isDefined) {
     return jsx(tag as any, {
-      dangerouslySetInnerHTML: {
-        __html: renderToString(content),
-      },
-      suppressHydrationWarning: true,
+      ...filteredProps,
+      children: content,
     }, `${id}-ssr`);
   }
   else {
